878c3e2154d084a3238d5cd161c7644c596243e1
Mark Lawson and Stephen Burden <mark@>
2016-09-22 14:02:24 -0700

    add navbar and some styling to home page

--- web/index.php
+++ web/index.php
@@ -9,6 +9,8 @@
         <meta charset="utf-8">
         <link href='/css/bootstrap.css' rel='stylesheet' type='text/css'>
         <link href='/css/styles.css' rel='stylesheet' type='text/css'>
+        <script src='/js/jquery-3.1.0.js'></script>
+        <script src='/js/bootstrap.js'></script>
         <title>Relp 2.0</title>
     </head>
 </html
