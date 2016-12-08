1bf380de6bf1439865b7b34d26d9dc436b49f871
Stephen Burden <spburden@hotmail.com>
2016-12-08 00:19:06 -0800

    change db setup2

--- web/index.php
+++ web/index.php
@@ -7,12 +7,12 @@
 <html>
     <head>
         <meta charset="utf-8">
-        <link href='css/bootstrap.css' rel='stylesheet' type='text/css'>
-        <link href='css/styles.css' rel='stylesheet' type='text/css'>
+        <link href='/css/bootstrap.css' rel='stylesheet' type='text/css'>
+        <link href='/css/styles.css' rel='stylesheet' type='text/css'>
         <link href="https://fonts.googleapis.com/css?family=Bitter" rel="stylesheet">
-        <script src='js/jquery-3.1.0.js'></script>
-        <script src='js/bootstrap.js'></script>
-        <script src='js/scripts.js'></script>
+        <script src='/js/jquery-3.1.0.js'></script>
+        <script src='/js/bootstrap.js'></script>
+        <script src='/js/scripts.js'></script>
         <title>Relp 2.0</title>
     </head>
 </html
